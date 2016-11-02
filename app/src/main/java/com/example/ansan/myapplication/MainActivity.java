cc03e65a9ae0c0e2637b93b8f4d3b379e4ec7003
bhj3729 <bhj3729@naver.com>
2016-11-02 12:43:50 +0900

    SD card 폴더 생성 함수

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.ansan.myapplication;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.os.EnvironmentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +23,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View v) { //button onclick걸기
+        //sd card사용 가능 여부 확인
+        if(isStoragePermissionGranted()){
+            Toast.makeText(getApplicationContext(),
+                     "SD card 사용불가", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        String folder = path + "/myLoveDir";
+        String filename = folder + "/myFile.txt";
+
+        File myfolder = new File(folder);
+
         switch (v.getId()) {
             //폴더 생성
             case R.id.button:
+                //디렉토리 만든는 함수.
+                myfolder.mkdir();
+                Toast.makeText(getApplicationContext(),
+                        "폴더 생성", Toast.LENGTH_SHORT).show();
                 break;
+
             //폴더 삭제
             case R.id.button2:
+
                 break;
             //파일 생성
             case R.id.button3:
